6882529fc0f0c37fea866ad3c243fe61d805e0f8
QuanKhanz <103108829+QuanKhanz@users.noreply.github.com>
2023-04-14 09:35:45 +0700

    add file sql insert DiaChi, NhanVien, KhachHang

---
+++ src/sql/insertData/NhanVien.sql
@@ -0,0 +1,18 @@
+INSERT INTO nhanvien (maNhanVien, hoTen, soDienThoai, ngaySinh, email, chucVu, maQuanLy, maDiaChi)
+VALUES
+	('NV0003', N'Phạm Hồng Đăng', '0918234567', '1985-08-11', 'dang.pham@example.com', 'Quản lý', 'NV0003', 'DC0002'),
+	('NV0001', N'Nguyễn Thị Hồng Nhung', '0987654321', '1995-05-23', 'nhung.nguyen@example.com', 'Nhân viên kinh doanh', 'NV0003', 'DC0000'),
+	('NV0002', N'Lê Văn Dương', '0909123456', '1990-01-12', 'duong.le@example.com', 'Nhân viên kế toán', 'NV0003', 'DC0001'),
+	('NV0004', N'Trần Thị Bích Phương', '0976543210', '1992-11-27', 'phuong.tran@example.com', 'Trưởng phòng kế toán', 'NV0003', 'DC0003'),
+	('NV0005', N'Nguyễn Minh Thảo', '0969123456', '1997-04-01', 'thao.nguyen@example.com', 'Nhân viên marketing', 'NV0003', 'DC0004'),
+	('NV0006', N'Phan Thị Thu Hiền', '0908123456', '1988-02-18', 'hien.phan@example.com', 'Nhân viên kinh doanh', 'NV0003', 'DC0005'),
+	('NV0007', N'Vũ Thị Hoài An', '0977654321', '1991-12-31', 'an.vu@example.com', 'Nhân viên marketing', 'NV0003', 'DC0006'),
+	('NV0008', N'Trần Văn Đức', '0912123456', '1995-03-25', 'duc.tran@example.com', 'Nhân viên kế toán', 'NV0003', 'DC0007'),
+	('NV0009', N'Nguyễn Thanh Thúy', '0988123456', '1993-07-09', 'thuy.nguyen@example.com', 'Nhân viên kinh doanh', 'NV0003', 'DC0008'),
+	('NV0010', N'Lê Hoàng Anh', '0969123456', '1998-09-10', 'anh.le@example.com', 'Nhân viên marketing', 'NV0003', 'DC0009'),
+	('NV0011', N'Trần Đức Anh', '0978123456', '1996-12-01', 'anh.tran@example.com', 'Nhân viên kế toán', 'NV0003', 'DC0010'),
+	('NV0012', N'Phạm Thanh Hương', '0912123456', '1990-05-15', 'huong.pham@example.com', 'Nhân viên kinh doanh', 'NV0003', 'DC0011'),
+	('NV0016', 'Lê Thị Thanh Trúc', '0987654321', '1988-02-12', 'trucle@gmail.com', 'Nhân viên bán hàng', 'NV0003', 'DC0005'),
+    ('NV0013', 'Phạm Thị Ngọc Yến', '0987123456', '1992-05-30', 'yentran@gmail.com', 'Nhân viên bán hàng', 'NV0003', 'DC0005'),
+    ('NV0014', 'Nguyễn Thị Trúc Phương', '0908123456', '1995-08-22', 'phuongnguyen2@gmail.com', 'Nhân viên kho', 'NV0003', 'DC0006'),
+    ('NV0015', 'Lê Hoàng Nam', '0978123456', '1986-06-25', 'namle@gmail.com', 'Nhân viên kế toán', 'NV0003', 'DC0006')
